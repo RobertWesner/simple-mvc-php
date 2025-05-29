884b96b46516b1f4a8e4ceee4597374e449047bc
RobertWesner <robert@wesner.io>
2025-05-29 15:45:04 +0200

    Added __BASE_DIR__ to route.php for use with dependency injection files

--- route.php
+++ route.php
@@ -1,11 +1,17 @@
 <?php
 
+/**
+ * @version v0.9.0
+ */
+
 declare(strict_types=1);
 
 use RobertWesner\SimpleMvcPhp\ErrorRenderer;
 use RobertWesner\SimpleMvcPhp\Routing\RouterFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+
+const __BASE_DIR__ = __DIR__;
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
