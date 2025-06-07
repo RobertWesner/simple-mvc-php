af8def7364edbc0d69ce1241feeb6875121dc3d7
RobertWesner <robert@wesner.io>
2025-06-07 23:43:14 +0200

    Allow non-container setups, fix incorrect typing

--- src/Routing/ContainerFactory.php
+++ src/Routing/ContainerFactory.php
@@ -10,7 +10,7 @@ final class ContainerFactory
 {
     private static ?Container $container = null;
 
-    public static function createContainer(): Container
+    public static function createContainer(): ?Container
     {
         if (class_exists(Container::class)) {
             self::$container = new Container();
@@ -19,7 +19,7 @@ final class ContainerFactory
         return self::$container;
     }
 
-    public static function getContainer(): Container
+    public static function getContainer(): ?Container
     {
         if (!isset(self::$container)) {
             die('Container was not created.');
